5126d284d1793e88f5e48d37cd993254d1f5ef5f
Leonardo200298 <leonardosanchez119@gmail.com>
2023-06-13 20:11:26 -0300

    feat: working in header and navbar

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	fontFamily:{
+		sans: ['Gotham SSm A, sans-serif']
+	},
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
